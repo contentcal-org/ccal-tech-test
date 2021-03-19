3e9f93d5608778cb93d4f05cd8d243869965ab84
Gwidon Walinski <gwidonw@gmail.com>
2021-03-19 15:33:07 +0100

    refactor: cleanup

--- front-end/apps/idea-board/src/app/app.module.ts
+++ front-end/apps/idea-board/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { UiModule } from '@ccal-apps/ui';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 
-const routes: Routes = [
-  { path: 'home', component: MainComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-];
+// const routes: Routes = [
+//   { path: 'home', component: MainComponent },
+//   { path: '', redirectTo: '/home', pathMatch: 'full' },
+// ];
 
 @NgModule({
-  declarations: [AppComponent, MainComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
