a8a7b6fea7899cdea84ad30c0104ee446ae5e0e9
Gwidon Walinski <gwidonw@gmail.com>
2021-03-22 11:06:47 +0100

    feat: add listing component placeholder

---
+++ front-end/apps/idea-board/src/app/ideas/_components/ideas-listing/ideas-listing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ccal-ideas-listing',
+  templateUrl: './ideas-listing.component.html',
+  styleUrls: ['./ideas-listing.component.scss'],
+})
+export class IdeasListingComponent {}
--- front-end/apps/idea-board/src/app/ideas/ideas.module.ts
+++ front-end/apps/idea-board/src/app/ideas/ideas.module.ts
@@ -6,9 +6,15 @@ import { IdeasListPage } from './ideas-list/ideas-list.page';
 import { IdeasEditPage } from './ideas-edit/ideas-edit.page';
 import { IdeasPage } from './ideas.page';
 import { RouterModule } from '@angular/router';
+import { IdeasListingComponent } from './_components/ideas-listing/ideas-listing.component';
 
 @NgModule({
-  declarations: [IdeasListPage, IdeasEditPage, IdeasPage],
+  declarations: [
+    IdeasListPage,
+    IdeasEditPage,
+    IdeasPage,
+    IdeasListingComponent,
+  ],
   imports: [CommonModule, RouterModule, IdeasRoutingModule],
 })
 export class IdeasModule {}
